0d9cf4ac1ef4d18836bafe8af34594e18270ca2c
Devashish Patel <devashishpatel01@gmail.com>
2023-07-06 22:19:13 +0530

    first version - non working

--- Counter/src/index.js
+++ Counter/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const display = document.getElementById("value");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function handleClick1() {
+  return (display.innerText = Number(display.innerText) + 1);
+}
+
+function Counter() {
+  return (
+    <div class="container">
+      <p class="heading">Counter</p>
+      <span id="value">0</span>
+      <div class="button-container">
+        <button class="dec" onClick={handleClick1}>
+          DECREASE
+        </button>
+        <button class="res" onClick={(display.innerText = 0)}>
+          RESET
+        </button>
+        <button
+          class="inc"
+          onClick={(display.innerText = Number(display.innerText) + 1)}
+        >
+          INCREASE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+root.render(<Counter />);
