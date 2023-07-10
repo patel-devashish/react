de9ac44512a330a3fe0d592dccb3c4939b65c526
Devashish Patel <devashishpatel01@gmail.com>
2023-07-10 23:25:14 +0530

    displaying value left, logic done

--- Counter/src/index.js
+++ Counter/src/index.js
@@ -2,28 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const display = document.getElementById("value");
+
+let v = 0;
 
 function handleClick1() {
-  return (display.innerText = Number(display.innerText) + 1);
+  console.log((v = v - 1));
+}
+
+function handleClick2() {
+  console.log((v = 0));
+}
+
+function handleClick3() {
+  console.log((v = v + 1));
 }
 
 function Counter() {
   return (
     <div class="container">
       <p class="heading">Counter</p>
-      <span id="value">0</span>
+      <span id="value">{v}</span>
       <div class="button-container">
         <button class="dec" onClick={handleClick1}>
           DECREASE
         </button>
-        <button class="res" onClick={(display.innerText = 0)}>
+        <button class="res" onClick={handleClick2}>
           RESET
         </button>
-        <button
-          class="inc"
-          onClick={(display.innerText = Number(display.innerText) + 1)}
-        >
+        <button class="inc" onClick={handleClick3}>
           INCREASE
         </button>
       </div>
