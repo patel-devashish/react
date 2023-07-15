9fcb2fac0b503d017cea15846b943010b9a56426
Devashish Patel <devashishpatel01@gmail.com>
2023-07-15 23:30:01 +0530

    template implemented

--- colorflipper/src/index.js
+++ colorflipper/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const colorsArr = [
+  "#FF0000",
+  "#FFFFFF",
+  "#00FFFF",
+  "#C0C0C0",
+  "#0000FF",
+  "#808080",
+  "#00008B",
+  "#000000",
+  "#ADD8E6",
+  "#FFA500",
+  "#800080",
+  "#A52A2A",
+  "#FFFF00",
+  "#800000",
+  "#00FF00",
+  "#008000",
+  "#FF00FF",
+  "#808000",
+  "#FFC0CB",
+  "#7FFFD4",
+];
+
+function Colorflipper() {
+  return (
+    <div>
+      <h2>
+        Background color: <span>#ffffff</span>
+      </h2>
+      <button>CLICK ME !</button>
+    </div>
+  );
+}
+root.render(<Colorflipper />);
